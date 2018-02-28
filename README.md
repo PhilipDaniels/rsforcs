719cd8cbdc25088d4dfddc61360eab475f0774e7
Philip Daniels <Philip.Daniels1971@gmail.com>
2018-02-28 22:04:30 +0000

    Single works as an adapter (try 1)

--- README.md
+++ README.md
@@ -17,3 +17,78 @@ available at that date.
 - [ ] Use https://github.com/mattico/elasticlunr-rs and add a search box.
 - [ ] Code files to be separate from the markdown? See https://github.com/verpeteren/gutenberg-materialize#options-index-page
 - [ ] Very nice code samples: https://www.fpcomplete.com/blog/2017/07/iterators-streams-rust-haskell
+
+
+## Pending material
+#### Build
+This doesn't handle code coverage but I always do 'watchexec -e rs "cargo test"'.
+
+
+#### Performance
+[profile.release]
+lto = true
+codegen-units = 1
+
+[profile.bench]
+lto = true
+codegen-units = 1
+
+[build]
+rustflags = ["-C",  "target-cpu=native"]
+
+#### Syntax
+Syntax
+iter: I           - immutable moved argument
+mut iter: I     - mutable moved argument
+
+
+#### Blog Improvements
+Side By Side Code Blocks
+
+<section class="container">
+    <div class="left">
+      <div class="code">
+        <p>
+        Some code
+        </p>
+        <p>
+        Some more code
+        </p>
+      </div>
+    </div>
+
+    <div class="right">
+      <div class="code">
+        <p>
+        Some right-hand-side code
+        </p>
+        <p>
+        Some more right-hand-side code
+        </p>
+        <p>
+        Yet more code
+        </p>
+      </div>
+    </div>
+</section>
+
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+}
+.left {
+  width: 50%;
+  order: 0;
+}
+.right {
+  width: 50%;
+  order: 1;
+}
+.code {
+  background-color: grey;
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid black;
+}
+
--- examples-rust/src/linq/single_tests.rs
+++ examples-rust/src/linq/single_tests.rs
@@ -1,21 +1,33 @@
 mod tests {
     use std::iter::Iterator;
 
-    /*
-    struct Single<I> where I : Iterator {
-        iter: I,
-        item: Option<I::Item>
-    }
+    // struct Single<I> where I : Iterator {
+    //     iter: I,
+    //     item: Option<I::Item>
+    // }
 
-    impl<I> Iterator for Single<I>
-        where I: Iterator
+    // impl<I> Iterator for Single<I>
+    //     where I: Iterator
+    // {
+    //     type Item = I::Item;
+
+    //     fn next(&mut self) -> Option<Self::Item> {
+    //         match self.iter.next() {
+    //             None => None,
+    //             Some(x) => match self.iter.next() {
+    //                 None => Some(x),
+    //                 Some(_) => None
+    //             }
+    //         }
+    //     }
+    // }
+
+    trait SingleIteratorAdapter : Iterator {
+        fn single(&mut self) -> Option<Self::Item>
     {
-        type Item = I::Item;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            match self.iter.next() {
+            match self.next() {
                 None => None,
-                Some(x) => match self.iter.next() {
+                Some(x) => match self.next() {
                     None => Some(x),
                     Some(_) => None
                 }
@@ -23,16 +35,7 @@ mod tests {
         }
     }
 
-    trait SingleIteratorAdapter : Iterator {
-        fn single(self) -> Single<Self>
-            where Self: Sized
-        {
-            Single { iter: self, item: None }
-        }
-    }
-
     impl<I> SingleIteratorAdapter for I where I : Iterator { }
-    */
 
     fn single_func<I: Iterator>(mut iter: I) -> Option<I::Item> {
         match iter.next() {
@@ -59,18 +62,18 @@ mod tests {
         assert_eq!(None, result);
     }
 
-    // #[test]
-    // fn single_ints_via_iterator_adapter() {
-    //     let source = vec![10];
-    //     let result = source.iter().single().unwrap();
-    //     assert_eq!(&10, result);
+    #[test]
+    fn single_ints_via_iterator_adapter() {
+        let source = vec![10];
+        let result = source.iter().single().unwrap();
+        assert_eq!(&10, result);
 
-    //     let source : Vec<i32> = vec![];
-    //     let result = source.iter().single();
-    //     assert_eq!(None, result);
+        let source : Vec<i32> = vec![];
+        let result = source.iter().single();
+        assert_eq!(None, result);
 
-    //     let source = vec![10, 20];
-    //     let result = source.iter().single();
-    //     assert_eq!(None, result);
-    // }
+        let source = vec![10, 20];
+        let result = source.iter().single();
+        assert_eq!(None, result);
+    }
 }
